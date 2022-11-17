f85459dc5639a90dc570a473272efe01408112d6
David02P <pietrocoladavid@gmail.com>
2022-11-17 16:34:19 -0500

    Creating users with the model

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,5 +1,16 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+//const {BadRequestError} = require('../errors')
+
 const register = async (req, res) => {
-  res.send("register user");
+  const {name,email,password} = req.body;
+  //! We can use a validator directly in the controller by throwing an error if an element is missing
+  // if(!name||!email||!password){
+  //   throw new BadRequestError('Please provide name email and password')
+  // }
+
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
