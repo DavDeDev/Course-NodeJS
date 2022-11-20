963023325b25207c119aa96e598977bc6768f08c
David02P <pietrocoladavid@gmail.com>
2022-11-19 23:14:02 -0500

    feat: Handling duplicates email

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -1,10 +1,28 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+  let customError = {
+    // If the error is already registered we will use those values, if not we gonna set a generic one
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong try again later",
+  };
 
-module.exports = errorHandlerMiddleware
+  // ! After we add the customError
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+
+  if (err.code && err.code === 11000) {
+    //! if the error exist and it's the one of email already registered
+    
+    //? Object.keys give an array of the keys in the JSON
+    customError.msg = `Duplicate value entered for ${
+      Object.keys(err.keyValue)[0]
+    } field, please choose another value`;
+    customError.statusCode = 400;
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
